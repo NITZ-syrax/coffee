d8470a941b12bbf549db404732923024ae1c0d43
NITZ-syrax <nitinrao2405@gmail.com>
2026-02-18 13:47:17 +0530

    Add files via upload

---
+++ coffeeee.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>cafe</title>
+    <link rel="stylesheet" href="coffeeee.css">
+    <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Latest compiled and minified CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Latest compiled JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    
+<div class="header">
+    
+    <div class="sec1">
+        <table>
+            <tr>
+                
+            </tr>
+        </table>
+    </div>
+
+    <div class="sec2">
+        <nav>
+            <ul>
+                <li class="nav-items"><a href="home.php">Home</a></li>
+                <li class="nav-items"><a href="menu.php">Menu</a></li>
+                <li class="nav-items"><a href="story.php">Story & Trust</a></li>
+                <li class="nav-items"><a href="blog.php">Blog</a></li>
+                <li class="nav-items"><a href="event.php">Brew Your Celebration</a></li>
+                <li class="nav-items"><a href="shop.php">Shop</a></li>
+                <li class="nav-items"><a href="membership.php">Artisan Membership</a></li>
+            </ul>
+        </nav>
+    </div>
+    </div>
+    <div class="mid-sec">
+        <video class="mid-bg" autoplay muted loop>
+        <source src="coffeebg.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+        </video>
+       <div class="container">
+
+       
+            <img src="spillcoffeenobg3.png" alt="Coffee cup spilling" class="coffee">
+        <img src="coffeeplatenobg.png" alt="White plate" class="plate">
+
+        <div class="mid mid1">
+            <div class="img-content">
+           <p><img src="newgoldleafnobg.png" alt="gold-leaf" class="leaf leaf1"></p>
+            <p class="t t1">Soft golden moments,</p>
+            <p class="t t2">gently brewed in amber silence.</p>
+            <p class="t t3">Where time slows, gently.</p>
+            <p class="t t4">Your calm between the chaos.</p>
+           <p><img src="newgoldleafnobg.png"  alt="gold-leaf2" class="leaf leaf2"></p>
+           </div>
+        </div>
+        <div class="mid mid2">
+           <div class="tagline1"> 
+        <p class="tagline1-text1">Time for</p>
+          <li class="tagline1-text2">Coffee</li>
+        </div>
+        
+        <div class="tagline2">
+            <li class="line" data-width="100%">VARIETIES CAREFULLY</li>
+            <li class="line" data-width="86%">SELECTED FOR YOU</li>
+            <li class="line" data-width="70%">BECAUSE TASTING IS PART</li>
+            <li class="line" data-width="55%">OF THE PROCESS.</li>
+        </div>
+        </div>
+        <div class="mid mid3">
+            
+        </div>
+        </div>
+          
+    </div>
+
+    <div class="below-mid">
+        <div class="bw-1">
+                <p class="featured">FEATURED SPECIAL</p>
+                <p class="loved">A showcase of our most loved creations.</p>
+                <p class="indulgence">offering indulgence wrapped in warmth and stillness.</p>
+                <p class="intro-of-best-seller intro1">At The Golden Hush, our featured selections are a gentle celebration of flavour and</p>
+                <p class="intro-of-best-seller intro2"> craftmanship. From velvety slow-brewed coffees to our signature handcrafted frappuccino,</p>
+                <p class="intro-of-best-seller intro3"> each creation is prepared with patience and intention. Paired with indulgent, freshly baked</p>
+                <p class="intro-of-best-seller intro4"> delights from Levain Bakery, every offering is designed to bring warmth, calm, and a quietly luxurious moment to your day.</p>
+                <p class="button"><a href="shop.php">Savour The Luxury</a></p>
+        </div>
+        <div class="bw-2">
+            <div class="best-seller-img">
+            <img src="frappe.png" alt="signature" class="signature-frappuccino">
+        </div>
+        </div>
+    </div>
+
+    <div class="shop-my-brand">
+        <div class="brand-img">
+            <img src="luxeblend.png" alt="our-brand" class="luxe-blend">
+        </div>
+        <div class="order-now">
+            <p class="buy">Grab a Bag of our Best Brew!</p>
+              <p class="grabing-lines gl1">A premium pour adorned with Royalty.</p>
+              <p class="grabing-lines gl2">Luxury isn’t served, it’s experienced as a ritual.</p>
+              <p class="tagline-of-shop">Bring the calm of Golden Hush into your home. Every 12oz bag carries handcrafted flavour, golden warmth, and moments worth slowing down for. <strong>Brew softly. Live richly.</strong></p>
+              <hr class="divided-line">
+              <p class="text-between-divider"><img src="coffee-cup-img.png"> 12 oz bag • 17-21 Servings</p>
+              <hr class="divided-line">
+              <div class="shop-today">
+              <a href="shop.php" class="order-it">699 Rs  |  Order Today</a>
+              </div>
+        </div>
+    </div>
+
+    <div class="bakery-section">
+        <div class="pastry-brand">
+             <p class="bakery-lines">Artisan Bakes, Sweet Pastries & Gourmet Sandwiches</p>
+             <p class="crafted">Crafted in Harmony: The</p>
+             <p class="Levain"> Golden Hush × Levain</p>
+             <p class="baked">Bakery</p>
+             <p class="brief-of-bakery">Every pastry we serve is handcrafted by Hunny Bunny Bakery, a local</p>
+             <p class="brief-of-bakery">gem we’re proud to support. It’s a sweet partnership that supports</p>
+             <p class="brief-of-bakery">local business and builds up Buckeye—one bite at a time. It’s more</p>
+             <p class="brief-of-bakery">than a treat—it’s a taste of Buckeye’s small business spirit, made fresh</p>
+             <p class="brief-of-bakery">just for you.</p>
+             <a href="bakery.php">Order now</a>
+
+        </div>
+        <div class="pastry-pic">
+             <img src="redvelvetpastry.png" alt="pastry-&-cookies">
+        </div>
+    </div>
+
+    <div class="barista">
+        <div class="basrita-pic">
+             <img src="https://images.stockcake.com/public/3/d/4/3d4012f5-d523-4144-bb82-6eb14a996b74_large/barista-serving-coffee-stockcake.jpg" alt="barista-img" class="barista-serve">
+        </div>
+
+        <div class="barista-describe">
+            <p class="barista-inspiration">Aspiring to create value in the workplace and meaningful change in the community.</p>
+            <p class="tagline-of-barista1">Launch Your Potential Here.</p>
+            <p class="tagline-of-barista2">Where Opportunity Meets Ambitions.</p>
+            <li class="tagline-spacing">Behind every cup we serve, a story of growth</li>
+            <li>quietly unfolds. This isn’t just a workplace—it’s a space where</li>
+            <li>young adults gain real job experience while learning</li>
+            <li>teamwork, responsibility, and confidence. Through</li>
+            <li>hands-on training and daily challenges, they</li>
+            <li>build valuable skills and prepare for life ahead. With every</li>
+            <li>task they master and every shift they complete,</li>
+            <li>they move closer to stability, independence, and a</li>
+            <li>brighter future.</li>
+        </div>
+    </div>
+
+    <div class="story-time">
+        <div class="read-our-story">
+             <p>Renewed craft. Refined taste. Reviving moments—one pour forward.</p>
+             <li>Golden Hush is more than a café — it’s where handcrafted coffee supports second chances, builds meaningful careers, and brings people together through shared purpose.</li>
+             <a href="story.php">Unveil The Journey</a>
+        </div>
+        <div class="cafe-pic">
+             <img src="https://i0.wp.com/cornerperk.com/wordpress/wp-content/uploads/2023/08/14-Questions-Every-Coffee-Enthusiast-Needs-to-Ask-the-Barista.jpg?fit=900%2C675&ssl=1" alt="baristaaa">
+        </div>
+    </div>
+
+    <div class="blog">
+        <div class="the-drip1">
+
+        </div>
+        <div class="the-drip2">
+            
+        </div>
+    </div>
+
+     
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
+    <script src="project1.js"></script>
+</body>
+</html>
